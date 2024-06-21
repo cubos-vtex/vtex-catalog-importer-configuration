4b4b21244668ecdb5ba692424c1b609758019115
Tiago de Andrade Freire <tiago.freire@cubos.io>
2024-06-21 01:25:23 -0300

    fix: removing originAccount validation

--- node/index.ts
+++ node/index.ts
@@ -1,19 +1,16 @@
-import type { ServiceContext } from '@vtex/api'
+import type { Maybe, ServiceContext } from '@vtex/api'
 import { ForbiddenError, Service, method } from '@vtex/api'
 
 export default new Service({
   routes: {
     settings: method({
       GET: async (context: ServiceContext) => {
-        const {
-          'user-agent': userAgent,
-          'x-vtex-origin-account': originAccount,
-        } = context.headers as Record<string, string | undefined>
+        const { 'user-agent': userAgent } = context.headers as Record<
+          string,
+          Maybe<string>
+        >
 
-        if (
-          !userAgent?.includes('ssesandbox04.catalog-importer') ||
-          !originAccount
-        ) {
+        if (!userAgent?.includes('ssesandbox04.catalog-importer')) {
           throw new ForbiddenError('Unauthorized access')
         }
 
